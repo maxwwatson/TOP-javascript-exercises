6be73f7899212d22e4a8973016b759eb683ef378
Max <max.w1@hotmail.com>
2023-12-24 11:35:14 +0000

    Exercise 1/2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    let output = "";
+    if(repeat < 0) {
+        return "ERROR";
+    }
+    for(let i = 0; i < repeat; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
