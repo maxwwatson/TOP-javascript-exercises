46bf6f8cd52141f87d7b6fa4c4d1ac0cfcb56d22
Max <max.w1@hotmail.com>
2023-12-24 14:05:46 +0000

    Done all exercises up to 7. All that is required for now in the course

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // f to c is (f - 32) * 5/9
+  let converted = (temp-32) * 5 / 9;
+  return parseFloat(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    // c to f is (0c x 9/5) + 32)
+    let converted = (temp * 9 / 5) + 32;
+    return parseFloat(converted.toFixed(1));
 };
 
 // Do not edit below this line
