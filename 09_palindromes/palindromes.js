fabee413cfba885506cd39b35f04a515942a3346
Max <max.w1@hotmail.com>
2023-12-28 18:07:28 +0000

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    // charCodeAt()
+    // char values - lowercase 97(a) - 122(z)
+    let lower = palindrome.toLowerCase();
+    let charArray = lower.split('');
+    //let evenString = (charArray.length%2) == 0 ? true : false;
+    // get rid of all whitespace and etc
+    let newStr = charArray.reduce(function(array, currentChar) {
+        if((currentChar.charCodeAt(0) < 97 || currentChar.charCodeAt(0) > 122)
+           && (currentChar.charCodeAt(0) < 48 || currentChar.charCodeAt(0) > 57)) {
+            return array;
+        };
+        return [...array, currentChar];
+    }, []);
+    console.log(newStr);
+    for(let i = 0; i <= newStr.length/2; i++) {
+        //console.log(`i value: ${newStr[i]} othervalue ${newStr[length]}`)
+        if(newStr[i] != newStr[newStr.length-1-i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
