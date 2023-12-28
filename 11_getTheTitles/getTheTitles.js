b849ac4caf14421075a6a58809c97734d5046a08
Max <max.w1@hotmail.com>
2023-12-28 19:25:22 +0000

    completed 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(theBooks) {
+    let outputArray = theBooks.map(function(array) {
+        return array.title;
+    });
+    console.log(outputArray);
+    return outputArray;
 };
 
 // Do not edit below this line
