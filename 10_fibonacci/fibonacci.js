0d11f15955d47815b517cbf7ae417b098f160bad
Max <max.w1@hotmail.com>
2023-12-28 18:27:49 +0000

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(nums) {
+    // create fibonacci sequence based on input nums, store in array
+    if(nums < 0) return "OOPS";
+    if(nums == 0) return 0;
+    if(nums == 1) return 1;
+    if(nums == 2) return 1;
+    let returnArray = [1,1];
+    for(let i = 2; i < nums; i++) {
+        returnArray.push(returnArray[i-1] + returnArray[i-2]);
+    }
+    
+    console.log(returnArray);
+    return returnArray[nums-1];
 };
 
 // Do not edit below this line
